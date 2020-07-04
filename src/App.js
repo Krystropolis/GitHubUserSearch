f3d8fd4ddc3aa0f0618e3532b7602b9a6bb8f227
Krystal <Krystropolis@users.noreply.github.com>
2020-07-04 12:37:36 -0700

    restructure of app. added pagination. removed searchByLogin. error handling on fetch.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import React, { Component } from 'react';
 import SearchInput from './SearchInput';
 import SearchResults from './SearchResults';
+import ReactPaginate from 'react-paginate';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       searchResults: null,
+      offset: 0,
+      data: [],
+      elements: [],
+      perPage: 10,
+      currentPage: 0,
     };
   }
 
+  // sets elements, passed to search result
+  setElementsForCurrentPage() {
+    let elements = this.state.searchResults.items.slice(
+      this.state.offset,
+      this.state.offset + this.state.perPage,
+    );
+    this.setState({ elements: elements });
+  }
+
+  // handles pagination
+  handlePageClick = data => {
+    const selectedPage = data.selected;
+    const offset = selectedPage * this.state.perPage;
+    this.setState({ currentPage: selectedPage, offset: offset }, () => {
+      this.setElementsForCurrentPage();
+    });
+  };
+
+  // handles search input
   handleSearchChange = value => {
     if (value) {
-      fetch('https://api.github.com/search/users?q=' + value + '+in:login')
-        .then(res => {
-          return res.json();
+      fetch('https://api.github.com/search/users?q=' + value)
+        .then(resp => resp.json())
+        .then(result => {
+          this.setState({ searchResults: result });
+          this.setElementsForCurrentPage();
         })
-        .then(res => {
-          this.setState({ searchResults: res });
+        .catch(err => {
+          console.error('Error:', err);
         });
     } else {
       this.setState({ searchResults: null });
@@ -25,10 +52,48 @@ class App extends Component {
   };
 
   render() {
+    // set up totalHits
+    let displayTotal, totalHits;
+    let noResults = null;
+    let count = 0;
+    if (
+      this.state.searchResults &&
+      this.state.searchResults.total_count !== undefined
+    ) {
+      count = this.state.searchResults.total_count;
+      if (count === 0) {
+        noResults = <div>No results found.</div>;
+      }
+    }
+    totalHits = <div key="totalHitsKey">Total hits: {count}</div>;
+    displayTotal = [noResults, totalHits];
+
+    // set up pagination when greater than 1 page of results
+    let paginationElement;
+    const pages = count > 0 ? Math.ceil(count / this.state.perPage) : 0;
+    if (pages > 1) {
+      paginationElement = (
+        <ReactPaginate
+          previousLabel={'<'}
+          nextLabel={'>'}
+          breakLabel={'...'}
+          pageCount={pages}
+          marginPagesDisplayed={1}
+          pageRangeDisplayed={5}
+          onPageChange={this.handlePageClick}
+          containerClassName={'pagination'}
+          subContainerClassName={'pages pagination'}
+          activeClassName={'active'}
+        />
+      );
+    }
+
     return (
       <div>
         <SearchInput textChange={this.handleSearchChange} />
-        <SearchResults searchResults={this.state.searchResults} />
+        {displayTotal}
+        <SearchResults searchResults={this.state.elements} />
+        {paginationElement}
       </div>
     );
   }
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -3,39 +3,47 @@ import PropTypes from 'prop-types';
 import SearchResultRow from './SearchResultRow';
 
 export default class SearchResults extends Component {
-  static propTypes = {
-    searchResults: PropTypes.object,
-  };
+	static propTypes = {
+		searchResults: PropTypes.array,
+	};
 
-  render() {
-    if (
-      this.props.searchResults &&
-      this.props.searchResults.total_count != undefined
-    ) {
-      let resultString =
-        this.props.searchResults.total_count === 0 ? 'No results found.' : '';
-      return (
-        <div className="total-data-results">
-          <div className="total-result-count">
-            <div>{resultString}</div>
-            Total hits: {this.props.searchResults.total_count}
-          </div>
-          <div className="user-data-results">
-            {this.props.searchResults.items.map(user => (
-              <SearchResultRow
-                key={user.id}
-                login={user.login}
-                avatar_url={user.avatar_url}
-                followers_url={user.followers_url}
-                html_url={user.html_url}
-                organizations_url={user.organizations_url}
-              />
-            ))}
-          </div>
-        </div>
-      );
-    } else {
-      return <div className="total-data-results">Total hits: 0</div>;
-    }
-  }
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			elements: [],
+		};
+	}
+
+	// turns search results into rows
+	createRowsForCurrentPage() {
+		let elements = this.props.searchResults.map(user => (
+			<SearchResultRow
+				key={user.id}
+				login={user.login}
+				avatar_url={user.avatar_url}
+				followers_url={user.followers_url}
+				html_url={user.html_url}
+				organizations_url={user.organizations_url}
+			/>
+		));
+		this.setState({ elements: elements });
+	}
+
+	render() {
+		return (
+			<div className="user-data-results">
+				{this.props.searchResults.map(user => (
+					<SearchResultRow
+						key={user.id}
+						login={user.login}
+						avatar_url={user.avatar_url}
+						followers_url={user.followers_url}
+						html_url={user.html_url}
+						organizations_url={user.organizations_url}
+					/>
+				))}
+			</div>
+		);
+	}
 }

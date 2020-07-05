3523fc5017c017e093752d514e42b93699842051
Krystal <Krystropolis@users.noreply.github.com>
2020-07-04 20:31:21 -0700

    add twitter handle, add react icons

--- src/SearchResultRow.js
+++ src/SearchResultRow.js
@@ -1,39 +1,79 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './SearchResultRow.css';
+import { FaTwitter } from 'react-icons/fa';
 
 export default class SearchResultsRow extends Component {
   static propTypes = {
     id: PropTypes.number,
-    login: PropTypes.string,
-    avatar_url: PropTypes.string,
-    followers_url: PropTypes.string,
-    html_url: PropTypes.string,
-    organizations_url: PropTypes.string,
+    user: PropTypes.string,
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      userData: null,
+    };
+  }
+
+  getUserInfo() {
+    fetch('https://api.github.com/users/' + this.props.user)
+      .then(resp => resp.json())
+      .then(result => {
+        this.setState({ userData: result });
+      })
+      .catch(err => {
+        console.error('Error:', err);
+      });
+  }
+
+  componentDidMount() {
+    this.getUserInfo();
+  }
+
   render() {
-    return (
-      <div className="user-row">
-        <div>
-          <a className="component-user-result-row" href={this.props.html_url}>
-            <img
-              alt={this.props.login}
-              src={this.props.avatar_url}
-              className="avatar"
-            />
-            <span className="login">{this.props.login}</span>
+    if (this.state.userData) {
+      let twitterComponent = null;
+      if (this.state.userData.twitter_username) {
+        twitterComponent = (
+          <a
+            href={`https://twitter.com/${this.state.userData.twitter_username}`}
+            className={'twitter-follow-button'}
+            data-size={'large'}
+            data-show-count={'false'}
+          >
+            <FaTwitter />
+            Follow {this.state.userData.twitter_username}
           </a>
+        );
+      }
+      return (
+        <div className="user-row">
+          <div>
+            <a
+              className="component-user-result-row"
+              href={this.state.userData.html_url}
+            >
+              <img
+                alt={this.state.userData.login}
+                src={this.state.userData.avatar_url}
+                className="avatar"
+              />
+              <span className="login">{this.state.userData.login}</span>
+            </a>
+          </div>
+          <div>
+            <span>Followers: {this.state.userData.followers}</span>
+            <span>Following: {this.state.userData.following}</span>
+          </div>
+          <div>
+            {this.state.userData.name}
+            {this.state.userData.bio}
+          </div>
+          {twitterComponent}
         </div>
-        <div>
-          <span>Followers: </span>
-          {this.props.followers_url}
-        </div>
-        <div>
-          <span>Organizations: </span>
-          {this.props.organizations_url}
-        </div>
-      </div>
-    );
+      );
+    }
+    return <div>Loading...</div>;
   }
 }
